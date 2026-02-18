8b9b37e82fb6560cc79d3965e8821db07c058596
ASDdevelop1087 <asd.develop1087@gmail.com>
2026-02-18 16:10:52 +0500

    added ability to go right and left and delete and also an indicator to show position

--- text_editor.py
+++ text_editor.py
@@ -5,12 +5,15 @@ import string
 # This creates a tuple of 'abcdef...XYZ...0123...'
 allowed_chars = tuple(string.ascii_letters + string.digits + string.punctuation + " ")
 
+note1 = ""
+note2 = ""
+
 def display_text(txt):
-    global note
+    global final_note,note1,note2
     os.system('cls')
-    note += txt
-    print(note)
-note = ""
+    note1 += txt
+    print(note1 + "|" + note2)
+    
 while True:
 
     event = keyboard.read_event()
@@ -19,11 +22,30 @@ while True:
         if event.name in allowed_chars:
             
             display_text(event.name)
+            
         elif event.name == "space":
+            
             display_text(" ")
+            
         elif event.name == "backspace":
-            note = note[:-1]
+            
+            note1 = note1[:-1]
             display_text("")
+            
+        elif event.name == "left":
+            if note1 != "":
+                
+                note2 = note1[-1] + note2
+                note1 = note1[:-1]
+                display_text("")
+                
+        elif event.name == "right":
+            if note2 != "":
+                
+                note1 += note2[0]
+                note2 = note2[1:]
+                display_text("")
+
             
             
             
